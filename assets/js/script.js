47c690e0a24eca2afd0517d9e4f1da16c8b2944e
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-04-04 10:04:05 -0300

    if

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,12 @@ const clock = setInterval(function time() {
     let min = dateToday.getMinutes();
     let sec = dateToday.getSeconds();
 
+    if (hour < 10) hour = '0' + hour;
+
+    if (min < 10) min = '0' + min;
+
+    if (sec < 10) sec = '0' + sec;
+
     hours.textContent = hour;
     minutes.textContent = min;
     seconds.textContent = sec;
